dcd161cb7f6a7030965f105edfbc4846c1179b3f
vanichka <vanichkatv@yandex.ru>
2022-11-01 14:40:43 +0500

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainClass {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+}
