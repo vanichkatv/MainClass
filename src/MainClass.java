5f395c8251bc9c95f36e0c3b6e3ec22574957a0e
vanichka <vanichkatv@yandex.ru>
2022-11-01 22:25:19 +0500

    main

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,23 +1,13 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MainClass {
-    //    public static int SIZE = 3;
-//    public static int DOTS_TO_WIN = 3;
-//    public static final char DOT_EMPTY = '•';
-//    public static final char DOT_X = 'X';
-//    public static final char DOT_O = 'O';
-//    public static char[][] map;
-//    public static Scanner sc = new Scanner(System.in);
-//    public static Random rand = new Random();
-    //test1
-    public static void main(String[] args) {
-        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr3.length; i++) {
-            if (arr3[i] < 6)
-                arr3[i] = arr3[i] * 2;
-        }
-        System.out.println(Arrays.toString(arr3));
-    }
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
 }
